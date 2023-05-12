f215a653b27d09f616f83190fa9ac6ab3007eb1f
Hanbit Kang <hanbitkang95@gmail.com>
2023-05-12 23:37:51 +0900

    reset, css style

---
+++ src/shared/Router.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUp from "../pages/SignUp";
+import Login from "../pages/Login";
+import Layout from "../components/Layout/Layout";
+import Home from "../pages/Home";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,105 @@
+import { createGlobalStyle } from "styled-components";
+import background from "../assets/img/background.jpg";
+
+
+export const GlobalStyles = createGlobalStyle`
+    :root{
+        /* Color */
+        --color-black: #343638;
+        --color-white: #F7F7FF;
+        --color-light-white: #f7f9fb;
+        --color-gray: #fafafa;
+
+        --color-blue: #0827F5;
+        --color-light-gray: #e2ebf2;
+        --color-yellow: #8B7803; //user
+        --color-dark-red: #D60F01;
+        --color-red: #6C0000;
+        --color-green: #1C4211; //visitor
+
+        /* Font size */
+        --font-extra-large: 60px;
+        --font-large: 32px;
+        --font-medium: 20px;
+        --font-regular: 16px;
+        --font-small: 14px;
+        --font-micro: 12px;
+
+        /* Font weight */
+        --weight-bold: 700;
+        --weight-semi-bold: 600; 
+        --weight-regular: 400;
+        --weight-light: 300;
+
+        /* Size */
+        --size-extra-large: 200px;
+        --size-large: 150px;
+        --size-medium: 120px;
+        --size-small: 80px;
+        --size-border-radius: 10px;
+    }
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body{
+        font-family: 'NeoDunggeunmo', 'NeoDunggeunmo Code';
+        background-color: var(--color-black);
+        background: url(${background}) no-repeat;
+        background-size: cover;
+        color: var(--color-white);
+    }
+
+    button{
+        background-color: transparent;
+        cursor: pointer;
+        border: none;
+        outline: none;
+        padding: 0px;
+    }
+
+    input {
+        border: none;
+        background-image: none;
+        background-color: transparent;
+        box-shadow: none;
+        outline: none;
+    } 
+
+    ul{
+        list-style: none;
+        padding: 0;
+        margin: 0;
+    }
+
+    textarea {
+    border: none;
+    overflow: auto;
+    outline: none;
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+    box-shadow: none;
+    resize: none;
+}
+    
+`;
+
+// export const LogoLink = styled(Link)`
+//   text-decoration: none;
+//   color: var(--color-black);
+//   cursor: pointer;
+//   font-size: ${(props) => props.fontSize};
+//   font-weight: ${(props) => props.fontWeight};
+//   color: ${(props) => props.color};
+// `;
+
+// export const NavBarLink = styled(Link)`
+//   text-decoration: none;
+//   color: var(--color-black);
+//   cursor: pointer;
+//   font-size: ${(props) => props.fontSize};
+//   font-weight: ${(props) => props.fontWeight};
+// `;
